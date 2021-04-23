40b6d0191921ff7bd29416f4ea956300cb7c0479
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-04-23 11:14:34 +0200

    suspects, weapons y rooms organizados

--- src/clue.js
+++ src/clue.js
@@ -1,13 +1,99 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+	{ mrGreen: {
+  firstName: 'Jacob',
+  lastName: 'Green',
+  occupation: 'Entrepreneur',
+  age: 45,
+  description: 'He has a lot of connections',
+  image: '',
+  color: 'green'} },
+  
+    { drOrchid: {
+  firstName: 'Doctor',
+  lastName: 'Orchid',
+  occupation: 'Scientist',
+  age: 26,
+  description: ' PhD in plant toxicology. Adopted daughter or Mr. Boddy',
+  image: '',
+  color: 'white'} },
+  
+    { profPlum: {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  occupation: 'Designer',
+  age: 22,
+  description: 'Billionaire videogame designer',
+  image: '',
+  color: 'purple' } },
+  
+    { missScarlet: {
+  firstName: 'Kasandra',
+  lastName: 'Scarlet',
+  occupation: 'Actor',
+  age: 31,
+  description: 'She is an A-list movie star with a dark past',
+  image: '',
+  color: 'red'} },
+  
+    { mrsPeacock: {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  occupation: 'Socialité',
+  age: 36,
+  description: 'She is from a wealthy family and uses her status and money to earn popularity',
+  image: '',
+  color: 'blue'} },
+  
+    { mrMustard: {
+  firstName: 'Jack',
+  lastName: 'Msutard',
+  occupation: 'Retired Football player',
+  age: 62,
+  description: 'He is a former football player who tries to get by on his former glory',
+  image: '',
+  color: 'yellow'
+  } },
+];
 
-// Rooms Collection
-const roomsArray = [];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+    {name: 'rope', weight: 10},
+    {name: 'knife', weight: 8},
+    {name: 'candlestick', weight: 2},
+    {name: 'dumbell', weight: 30},
+    {name: 'poison', weight: 2},
+    {name: 'axe', weight: 15},
+    {name: 'bat', weight: 13},
+    {name: 'trophy', weight: 25},
+    {name: 'pistol', weight: 20},
+    
+];
+    
+
+// Rooms Collection
+const roomsArray = [
+    'Dining Room',
+    'Conservatory',
+    'Kitchen',
+    'Study',
+    'Library',
+    'Billiard Room',
+    'Lounge',
+    'Ballroom',
+    'Hall',
+    'Spa',
+    'Living Room',
+    'Observatory',
+    'Theater',
+    'Guest House',
+    'Patio'
+    
+];
+
 
 // ITERATION 2
 
